4dc1e76235b6a0db11f5f6a32cf11fdb56e75b72
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-02-27 11:31:32 +0100

    remove test example
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- src/network.c
+++ src/network.c
@@ -79,64 +79,6 @@ int_from_cmd(const char *cmd, const char *cmd_arg, const char *fmt, void *ptr)
     return rc;
 }
 
-int
-test()
-{
-    sr_conn_ctx_t *conn = NULL;
-    sr_session_ctx_t *sess = NULL;
-    sr_val_t value = { 0 };
-    int rc = SR_ERR_OK;
-    sr_log_stderr(SR_LL_DBG);
-
-    /* connect to sysrepo */
-    rc = sr_connect("app3", SR_CONN_DEFAULT, &conn);
-    if (SR_ERR_OK != rc) {
-        goto cleanup;
-    }
-
-    /* start session */
-    rc = sr_session_start(conn, SR_DS_STARTUP, SR_SESS_DEFAULT, &sess);
-    if (SR_ERR_OK != rc) {
-        goto cleanup;
-    }
-
-    /* create new interface named 'gigaeth0' of type 'ethernetCsmacd' */
-    value.type = SR_IDENTITYREF_T;
-    value.data.identityref_val = "ethernetCsmacd";
-    rc = sr_set_item(sess, "/ietf-interfaces:interfaces/interface[name='gigaeth0']/type", &value, SR_EDIT_DEFAULT);
-    if (SR_ERR_OK != rc) {
-      fprintf(stderr, "Error by sr_set_item (%s): %s\n", "/ietf-interfaces:interfaces/interface[name='gigaeth0']/type", sr_strerror(rc));
-        goto cleanup;
-    }
-
-    /* set 'prefix-length' leaf inside of the 'address' list entry with key 'fe80::ab8'
-       (list entry will be automatically created if it does not exist) */
-    value.type = SR_UINT8_T;
-    value.data.uint8_val = 64;
-    rc = sr_set_item(sess, "/ietf-interfaces:interfaces/interface[name='gigaeth0']/ietf-ip:ipv6/address[ip='fe80::ab8']/prefix-length",
-                     &value, SR_EDIT_DEFAULT);
-    if (SR_ERR_OK != rc) {
-      fprintf(stderr, "Error by sr_set_item (%s): %s\n", "/ietf-interfaces:interfaces/interface[name='gigaeth0']/ietf-ip:ipv6/address[ip='fe80::ab8']/prefix-length", sr_strerror(rc));
-        goto cleanup;
-    }
-
-    /* commit the changes */
-    rc = sr_commit(sess);
-    if (SR_ERR_OK != rc) {
-        printf("Error by sr_commit: %s\n", sr_strerror(rc));
-        goto cleanup;
-    }
-
-  cleanup:
-    if (NULL != sess) {
-        sr_session_stop(sess);
-    }
-    if (NULL != conn) {
-        sr_disconnect(conn);
-    }
-    return rc;
-}
-
 static int
 sysrepo_commit_network(sr_session_ctx_t *sess, struct if_interface *iface)
 {
@@ -380,7 +322,6 @@ sr_plugin_init_cb(sr_session_ctx_t *session, void **private_ctx)
     eth0->ipv4 = calloc(1, sizeof(struct ip_v4));
     init_config(eth0->ipv4);
     sysrepo_commit_network(session, eth0);
-    test();
 
     SRP_LOG_DBG_MSG("Plugin initialized successfully");
 
